2e4f1ac845646557b7e92dde242edafe5775bb32
Adam Brewer <adamwbrewer@me.com>
2023-01-09 11:03:17 -0500

    added code for second experiment and visualizations for first

--- Protein_Queries.sql
+++ Protein_Queries.sql
@@ -106,3 +106,150 @@ INNER JOIN Protein_Description AS PD on P.Description_ID = PD.Description_ID
 INNER JOIN Peptide AS PE on P.Peptide_ID = PE.Peptide_ID
 GROUP BY P.Protein_Name
 ORDER BY Hits DESC
+
+-- Ellie stuff
+
+SELECT 
+    G.Gene_Label,
+    COUNT(G.Gene_Label)/15 AS Protein_Repeats
+    FROM Mouse_Protein AS MP 
+INNER JOIN Mouse AS M on MP.Mouse_ID = M.Mouse_ID
+INNER JOIN Protein AS P on MP.Protein_ID = P.Protein_ID
+INNER JOIN Virus AS V on M.Virus_ID = V.Virus_ID
+INNER JOIN Biotin AS B on M.Biotin_ID = B.Biotin_ID
+INNER JOIN Fraction AS F on M.Fraction_ID = F.Fraction_ID
+INNER JOIN Protein_Id AS PI on P.Protein_Id_ID = PI.Protein_Id_ID
+INNER JOIN Gene AS G on P.Gene_ID = G.Gene_ID
+INNER JOIN Protein_Description AS PD on P.Description_ID = PD.Description_ID
+INNER JOIN Peptide AS PE on P.Peptide_ID = PE.Peptide_ID
+GROUP BY G.Gene_Label
+HAVING COUNT(G.Gene_Label)/15 > 1
+
+-- Ellie Part 2
+
+-- Shows all genes with protein repeats and specific fraction, virus, and biotin labels
+SELECT 
+    G.Gene_Label,
+    COUNT(G.Gene_Label) AS Protein_Repeats,
+    V.Virus_Label,
+    B.Biotin_Label,
+    F.Fraction_Label,
+    MP.Hits
+    FROM Mouse_Protein AS MP 
+INNER JOIN Mouse AS M on MP.Mouse_ID = M.Mouse_ID
+INNER JOIN Protein AS P on MP.Protein_ID = P.Protein_ID
+INNER JOIN Virus AS V on M.Virus_ID = V.Virus_ID
+INNER JOIN Biotin AS B on M.Biotin_ID = B.Biotin_ID
+INNER JOIN Fraction AS F on M.Fraction_ID = F.Fraction_ID
+INNER JOIN Protein_Id AS PI on P.Protein_Id_ID = PI.Protein_Id_ID
+INNER JOIN Gene AS G on P.Gene_ID = G.Gene_ID
+INNER JOIN Protein_Description AS PD on P.Description_ID = PD.Description_ID
+INNER JOIN Peptide AS PE on P.Peptide_ID = PE.Peptide_ID
+WHERE F.Fraction_Label = 'whole brain' AND B.Biotin_Label = '72' AND V.Virus_Label = 'TDP43-TurboID'-- AND MP.Hits > 0
+GROUP BY G.Gene_Label, V.Virus_Label,B.Biotin_Label,F.Fraction_Label,M.Sample_Name,MP.Hits
+ORDER BY Protein_Repeats DESC
+
+-- Shows all genes with protein repeats greater than one and that have hits greater or equal than zero
+-- this table shows all genes with specific fraction, biotin and virus labels with hits  that are equal to zero
+SELECT 
+    G.Gene_Label,
+    COUNT(G.Gene_Label) AS Protein_Repeats,
+    V.Virus_Label,
+    B.Biotin_Label,
+    F.Fraction_Label,
+    MP.Hits
+    FROM Mouse_Protein AS MP 
+INNER JOIN Mouse AS M on MP.Mouse_ID = M.Mouse_ID
+INNER JOIN Protein AS P on MP.Protein_ID = P.Protein_ID
+INNER JOIN Virus AS V on M.Virus_ID = V.Virus_ID
+INNER JOIN Biotin AS B on M.Biotin_ID = B.Biotin_ID
+INNER JOIN Fraction AS F on M.Fraction_ID = F.Fraction_ID
+INNER JOIN Protein_Id AS PI on P.Protein_Id_ID = PI.Protein_Id_ID
+INNER JOIN Gene AS G on P.Gene_ID = G.Gene_ID
+INNER JOIN Protein_Description AS PD on P.Description_ID = PD.Description_ID
+INNER JOIN Peptide AS PE on P.Peptide_ID = PE.Peptide_ID
+WHERE F.Fraction_Label = 'whole brain' AND B.Biotin_Label = '72' AND V.Virus_Label = 'TDP43-TurboID'-- AND MP.Hits > 0
+GROUP BY G.Gene_Label, V.Virus_Label,B.Biotin_Label,F.Fraction_Label,M.Sample_Name,MP.Hits
+HAVING COUNT(G.Gene_Label) > 1
+ORDER BY Protein_Repeats ASC
+
+-- Shows all genes with protein repeats greater than zero and that also have hits greater than zero
+-- this table shows all genes with specific fraction, biotin and virus labels with hits  that aren't equal to zero
+-- table you've been looking for!!!
+SELECT 
+    G.Gene_Label,
+    COUNT(G.Gene_Label) AS Protein_Repeats,
+    V.Virus_Label,
+    B.Biotin_Label,
+    F.Fraction_Label,
+    SUM(MP.Hits) AS Hits,
+    SUM(MP.Hits) / COUNT(G.Gene_Label) AS Hits_per_Protein
+    FROM Mouse_Protein AS MP 
+INNER JOIN Mouse AS M on MP.Mouse_ID = M.Mouse_ID
+INNER JOIN Protein AS P on MP.Protein_ID = P.Protein_ID
+INNER JOIN Virus AS V on M.Virus_ID = V.Virus_ID
+INNER JOIN Biotin AS B on M.Biotin_ID = B.Biotin_ID
+INNER JOIN Fraction AS F on M.Fraction_ID = F.Fraction_ID
+INNER JOIN Protein_Id AS PI on P.Protein_Id_ID = PI.Protein_Id_ID
+INNER JOIN Gene AS G on P.Gene_ID = G.Gene_ID
+INNER JOIN Protein_Description AS PD on P.Description_ID = PD.Description_ID
+INNER JOIN Peptide AS PE on P.Peptide_ID = PE.Peptide_ID
+WHERE F.Fraction_Label = 'whole brain' AND B.Biotin_Label = '72' AND V.Virus_Label = 'TDP43-TurboID'
+GROUP BY G.Gene_Label, V.Virus_Label,B.Biotin_Label,F.Fraction_Label
+HAVING COUNT(G.Gene_Label) > 0 AND SUM(MP.Hits) > 0
+ORDER BY Hits_per_Protein DESC
+
+
+
+-- Desired query visuals - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+-- TDP43-TurboID whole brain, 72hr top hits in all replicates
+
+SELECT 
+G.Gene_Label,
+COUNT(G.Gene_Label) AS Protein_Repeats,
+V.Virus_Label,
+B.Biotin_Label,
+F.Fraction_Label,
+SUM(MP.Hits) AS Hits,
+SUM(MP.Hits) / COUNT(G.Gene_Label) AS Hits_per_Protein
+FROM Mouse_Protein AS MP 
+INNER JOIN Mouse AS M on MP.Mouse_ID = M.Mouse_ID
+INNER JOIN Protein AS P on MP.Protein_ID = P.Protein_ID
+INNER JOIN Virus AS V on M.Virus_ID = V.Virus_ID
+INNER JOIN Biotin AS B on M.Biotin_ID = B.Biotin_ID
+INNER JOIN Fraction AS F on M.Fraction_ID = F.Fraction_ID
+INNER JOIN Protein_Id AS PI on P.Protein_Id_ID = PI.Protein_Id_ID
+INNER JOIN Gene AS G on P.Gene_ID = G.Gene_ID
+INNER JOIN Protein_Description AS PD on P.Description_ID = PD.Description_ID
+INNER JOIN Peptide AS PE on P.Peptide_ID = PE.Peptide_ID
+WHERE F.Fraction_Label = 'whole brain' AND B.Biotin_Label = '72' AND V.Virus_Label = 'TDP43-TurboID'
+GROUP BY G.Gene_Label, V.Virus_Label,B.Biotin_Label,F.Fraction_Label
+HAVING COUNT(G.Gene_Label) > 0 AND SUM(MP.Hits) > 0
+ORDER BY Hits_per_Protein DESC
+
+
+-- TurboID whole brain, 72hr top hits in all replicates
+
+SELECT 
+G.Gene_Label,
+COUNT(G.Gene_Label) AS Protein_Repeats,
+V.Virus_Label,
+B.Biotin_Label,
+F.Fraction_Label,
+SUM(MP.Hits) AS Hits,
+SUM(MP.Hits) / COUNT(G.Gene_Label) AS Hits_per_Protein
+FROM Mouse_Protein AS MP 
+INNER JOIN Mouse AS M on MP.Mouse_ID = M.Mouse_ID
+INNER JOIN Protein AS P on MP.Protein_ID = P.Protein_ID
+INNER JOIN Virus AS V on M.Virus_ID = V.Virus_ID
+INNER JOIN Biotin AS B on M.Biotin_ID = B.Biotin_ID
+INNER JOIN Fraction AS F on M.Fraction_ID = F.Fraction_ID
+INNER JOIN Protein_Id AS PI on P.Protein_Id_ID = PI.Protein_Id_ID
+INNER JOIN Gene AS G on P.Gene_ID = G.Gene_ID
+INNER JOIN Protein_Description AS PD on P.Description_ID = PD.Description_ID
+INNER JOIN Peptide AS PE on P.Peptide_ID = PE.Peptide_ID
+WHERE F.Fraction_Label = 'whole brain' AND B.Biotin_Label = '72' AND V.Virus_Label = 'TurboID'-- AND MP.Hits > 0
+GROUP BY G.Gene_Label, V.Virus_Label,B.Biotin_Label,F.Fraction_Label
+HAVING COUNT(G.Gene_Label) > 0 AND SUM(MP.Hits) > 0
+ORDER BY Hits_per_Protein DESC
